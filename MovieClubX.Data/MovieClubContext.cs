4bcf67c8245dc2a39de8e54b36083f637a5015e5
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2025-03-17 10:07:11 +0100

    identity tables added

--- MovieClubX.Data/MovieClubContext.cs
+++ MovieClubX.Data/MovieClubContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MovieClubX.Entities.Entity;
 
 namespace MovieClubX.Data
 {
-    public class MovieClubContext : DbContext
+    public class MovieClubContext : IdentityDbContext
     {
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Rate> Rates { get; set; }
